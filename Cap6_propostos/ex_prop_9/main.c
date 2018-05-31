6bd8bb521780507819900029c784c4ca74a1d21b
Thiago Henrique D. Botelho <thenrique2012@gmail.com>
2018-05-31 14:31:31 -0300

    Exercicios resolvidos em C

--- Cap6_propostos/ex_prop_9/main.c
+++ Cap6_propostos/ex_prop_9/main.c
@@ -12,8 +12,47 @@ de 15%; e para aqueles que satisfazem apenas a condição de preço, o aumento s
 
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 3
 
 int main(int argc, char** argv) {
+    char nome[TAM][40];
+    int i, c = 0, cod[TAM], indice_npreco[TAM];
+    float preco[TAM], n_preco;
+
+    for (i = 0; i < TAM; i++) {
+        printf("%dº Produto\n", i + 1);
+        printf("Nome do produto: ");
+        setbuf(stdin, NULL);
+        gets(nome[i]);
+        printf("Codigo: ");
+        scanf("%d", &cod[i]);
+        printf("Preço: ");
+        scanf("%f", &preco[i]);
+        printf("\n");
+    }
+
+    printf("RELATÓRIO\n\n");
+    for (i = 0; i < TAM; i++) {
+        printf("%dº Produto\n", i + 1);
+        printf("\tNome do produto: %s \n", nome[i]);
+        printf("\tCodigo: %d \n", cod[i]);
+        printf("\tPreço: R$ %.2f. \n", preco[i]);
+
+        c = 0;
+        if (cod[i] % 2 == 0 && preco[i] > 1000) {
+            n_preco = preco[i] * 1.20;
+            c++;
+        } else if (cod[i] % 2 == 0 && preco[i] < 1000) {
+            n_preco = preco[i] * 1.15;
+            c++;
+        } else {
+            n_preco = preco[i] * 1.10;
+            c++;
+        }
+        if (c > 0) {
+            printf("\tNovo preco: R$ %.2f. \n", n_preco);
+        }
+    }
 
     return (EXIT_SUCCESS);
 }
