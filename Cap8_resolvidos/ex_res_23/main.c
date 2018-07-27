9ce05fb498898dcaf302d43965ab1c7be831bd56
Thiago Henrique Domingues Botelho <39653866+thenriquedb@users.noreply.github.com>
2018-07-27 15:46:25 -0300

    Update main.c

--- Cap8_resolvidos/ex_res_23/main.c
+++ Cap8_resolvidos/ex_res_23/main.c
@@ -6,22 +6,66 @@ principal.
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-#define LIN 2
-#define COL 3
-#define TAM 12
+#define LIN 3
+#define COL 4
 
-void Prencher_Matriz(float mat[][COL]);
 
+//Função para prencher matriz
+void PrencherMatriz(float matriz[][COL]);
+
+//Função para imprimir matriz
+void ImprimirMatriz(float matriz[][COL]);
+
+//Funcao para passar matriz para vetor
+void Transformar_Em_Vetor(float matriz[][COL], float *vet);
 
 int main(int argc, char** argv) {
+    float mat[LIN][COL], vet[LIN * COL];
+    int i;
 
+    PrencherMatriz(mat);
+    ImprimirMatriz(mat);
+    Transformar_Em_Vetor(mat, &vet);
+
+    printf("\nMatriz gerada passada para um vetor:  \n");
+    for (i = 0; i < LIN * COL; i++) {
+        printf("%.2f - ",vet[i]);
+    }
 
     return (EXIT_SUCCESS);
 }
 
-void Prencher_Matriz(float mat[][COL]) {
+void PrencherMatriz(float matriz[][COL]) {
+    int i, j;
+    srand(time(NULL));
 
+    for (i = 0; i < LIN; i++) {
+        for (j = 0; j < COL; j++) {
+            printf("M[%d][%d]: ", i, j);
+            scanf("%f", &matriz[i][j]);
+        }
+    }
 }
 
+void ImprimirMatriz(float matriz[][COL]) {
+    int i, j;
+    printf("Matriz gerada: \n");
+    for (i = 0; i < LIN; i++) {
+        for (j = 0; j < COL; j++) {
+            printf("\t%.2f \t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void Transformar_Em_Vetor(float matriz[][COL], float *vet) {
+    int i, j, p = 0;
+
+    for (i = 0; i < LIN; i++) {
+        for (j = 0; j < COL; j++) {
+            vet[p] = matriz[i][j];
+            p++;
+        }
+    }
+}
